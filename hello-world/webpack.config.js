b7ad21f6b31933f8378039c2dfe99d675422385b
Alexei Mukovozchik <krisfragerfeed@gmail.com>
2021-04-29 14:16:35 +0300

    feat: cleared create-react-app source files

--- hello-world/webpack.config.js
+++ hello-world/webpack.config.js
@@ -1,9 +1,49 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
+    path: path.join(__dirname, 'build'),
     filename: 'main.js',
-    path: path.resolve(__dirname, 'build'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    port: 9000,
+    watchContentBase: true,
+    progress: true,
+    overlay: true,
+    open: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [path.resolve('node_modules'), 'node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
   },
 };
